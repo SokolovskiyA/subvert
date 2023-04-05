815f6ddd31d07b100f32d7256a49902705a9321f
Alexander Sokolovskiy <alexandersokolovskiy@Alexanders-MacBook-Pro-2.local>
2023-04-05 13:04:55 -0700

    ...

--- src/Components/SmallerComponents/Header/Header.jsx
+++ src/Components/SmallerComponents/Header/Header.jsx
@@ -9,6 +9,7 @@ import puppet from '../../../Assets/puppets/BrownPuppet.png';
 
 
 
+
 function Header() {
 
     const [ menuOpen, setMenuOpen ] = useState(false)
@@ -26,13 +27,11 @@ function Header() {
 
     
     return (
-        <motion.div initial={{height: "15vh"}} whileInView={{height: "20vh"}} transition={{duration: 1}} className="header">
+        <motion.div className="header">
             <Button hover={setHover} classN="header__button" click={openMenu} text="Menu"/>
-            <AnimatePresence>{menuOpen && <Navigation/>}</AnimatePresence> 
+            <AnimatePresence>{menuOpen && <Navigation/>}</AnimatePresence>
+            <motion.img whileInView={{rotate: 355}} transition={{duration: 1}} src={logo} className='header__logo' alt="logo"/>
             <motion.img animate={{margin: mouseOnButton ? "3.5rem 0 0 -2rem" : "3.5rem 0 0 -6rem"}} src={puppet} className='header__puppet' alt="puppet"/>
-            <div className='header__right'>
-                <motion.img initial={{height: 0}} whileInView={{ height: "75%",  rotate: 360}} transition={{duration: 1}} src={logo} className='header__logo' alt="logo"/>
-            </div>
         </motion.div> 
     )
 }
--- src/Components/SmallerComponents/Navigation/Navigation.jsx
+++ src/Components/SmallerComponents/Navigation/Navigation.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 
 
 
+
 function Navigation() {
     return (
         <motion.div initial={{width: 0}} animate={{width: "fit-content"}} exit={{width: 0}} transition={{duration: 2}} key="modal" className="nav">
@@ -12,6 +13,7 @@ function Navigation() {
             <Link className='nav__link' to="/videos"><motion.span className="nav__span" initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}}>More Video</motion.span></Link>
             <Link className='nav__link' to="/audio"><motion.span className="nav__span" initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}}>More Audio</motion.span></Link>
             <Link className='nav__link' to="/store"><motion.span className="nav__span" initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}}>More Merch</motion.span></Link>
+            
         </motion.div>
     )
 }
