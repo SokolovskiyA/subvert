3314119ea9e1d52ed621a23ec6e8af77463a5104
Alexander Sokolovskiy <alexandersokolovskiy@Alexanders-MacBook-Pro-2.local>
2023-04-05 12:35:10 -0700

    fixing bugs

--- src/Components/SmallerComponents/Navigation/Navigation.jsx
+++ src/Components/SmallerComponents/Navigation/Navigation.jsx
@@ -7,11 +7,11 @@ import { motion } from "framer-motion";
 
 function Navigation() {
     return (
-        <motion.div initial={{width: 0}} animate={{width: "30%"}} exit={{width: 0}} transition={{duration: 2}} key="modal" className="nav">
-            <Link className='nav__link' to="/home">Home</Link>
-            <Link className='nav__link' to="/videos">More Video</Link>
-            <Link className='nav__link' to="/audio">More Audio</Link>
-            <Link className='nav__link' to="/store">More Merch</Link>
+        <motion.div initial={{width: 0}} animate={{width: "fit-content"}} exit={{width: 0}} transition={{duration: 2}} key="modal" className="nav">
+            <Link className='nav__link' to="/home"><motion.span className="nav__span" initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}}>Home</motion.span></Link>
+            <Link className='nav__link' to="/videos"><motion.span className="nav__span" initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}}>More Video</motion.span></Link>
+            <Link className='nav__link' to="/audio"><motion.span className="nav__span" initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}}>More Audio</motion.span></Link>
+            <Link className='nav__link' to="/store"><motion.span className="nav__span" initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1}}>More Merch</motion.span></Link>
         </motion.div>
     )
 }
