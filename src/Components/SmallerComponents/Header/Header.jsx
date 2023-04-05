aeff8745bbc824c8ec02e8220e38aea44a6f6263
Alexander Sokolovskiy <alexandersokolovskiy@Alexanders-MacBook-Pro-2.local>
2023-04-05 12:23:56 -0700

    header changed animation fixed

--- src/Components/SmallerComponents/Header/Header.jsx
+++ src/Components/SmallerComponents/Header/Header.jsx
@@ -10,6 +10,7 @@ import puppet from '../../../Assets/puppets/BrownPuppet.png';
 
 
 function Header() {
+
     const [ menuOpen, setMenuOpen ] = useState(false)
     const [ mouseOnButton, setMouseOnButton ] = useState(false)
 
@@ -21,15 +22,13 @@ function Header() {
         e.preventDefault()
         setMenuOpen(true)
     }
-    const closeMenu = (e) => {
-        e.preventDefault()
-        setMenuOpen(false)
-    }
+
+
     
     return (
         <motion.div initial={{height: "15vh"}} whileInView={{height: "20vh"}} transition={{duration: 1}} className="header">
             <Button hover={setHover} classN="header__button" click={openMenu} text="Menu"/>
-            <AnimatePresence>{menuOpen && <Navigation click={closeMenu}/>}</AnimatePresence> 
+            <AnimatePresence>{menuOpen && <Navigation/>}</AnimatePresence> 
             <motion.img animate={{margin: mouseOnButton ? "3.5rem 0 0 -2rem" : "3.5rem 0 0 -6rem"}} src={puppet} className='header__puppet' alt="puppet"/>
             <div className='header__right'>
                 <motion.img initial={{height: 0}} whileInView={{ height: "75%",  rotate: 360}} transition={{duration: 1}} src={logo} className='header__logo' alt="logo"/>
--- src/Components/SmallerComponents/Navigation/Navigation.jsx
+++ src/Components/SmallerComponents/Navigation/Navigation.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navigation.scss'
-import Button from '../Button/Button'
 import { motion } from "framer-motion";
 
 
-function Navigation({click}) {
+
+function Navigation() {
     return (
-        <motion.div initial={{width: "0%"}} animate={{width: "30%"}} exit={{width: "0%"}} transition={{duration: 1}} key="modal" className="nav">
-            <Link className='nav__link' to="/home"><motion.span initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.5 ,duration: 1}}>Home</motion.span></Link>
-            <Link className='nav__link' to="/videos"><motion.span initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.5 ,duration: 1}}>More Video</motion.span></Link>
-            <Link className='nav__link' to="/audio"><motion.span initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.5 ,duration: 1}}>More Audio</motion.span></Link>
-            <Link className='nav__link' to="/store"><motion.span initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.5 ,duration: 1}}>More Merch</motion.span></Link>
-            <Button text="close" click={click} classN="nav__close"/>
+        <motion.div initial={{width: 0}} animate={{width: "30%"}} exit={{width: 0}} transition={{duration: 2}} key="modal" className="nav">
+            <Link className='nav__link' to="/home">Home</Link>
+            <Link className='nav__link' to="/videos">More Video</Link>
+            <Link className='nav__link' to="/audio">More Audio</Link>
+            <Link className='nav__link' to="/store">More Merch</Link>
         </motion.div>
     )
 }
