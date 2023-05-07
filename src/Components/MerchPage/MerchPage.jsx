4d9e7e163cf8b7d8efcc651815f775dcaee27bdb
SokolovskiyA <sokolovskiya12@gmail.com>
2023-05-07 13:44:20 -0700

    on the finish line. setting up merch

--- src/Components/MerchPage/MerchPage.jsx
+++ src/Components/MerchPage/MerchPage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './MerchPage.scss'
 import Header from '../SmallerComponents/Header/Header'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 function MerchPage() {
+    const url = 'https://api.printful.com/store/products'
+    const getProduct = () => {
+        axios.get( url, {  
+            headers: {
+                Authorization: 'Bearer DwLaoIRBYFlqE7JcaTMF7klB7LKzXkJueUThMPKv'
+            }
+        })
+        .then((res) => {
+            console.log(res.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
+
+    useEffect(() => {
+        getProduct()
+    }, [])
+
+
     return (
         <div>  
             <Header/>
