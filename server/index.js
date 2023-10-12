5a0f9dd2dacdcb3e2b65d5613693709e9d6bcca2
SokolovskiyA <sokolovskiya12@gmail.com>
2023-10-12 14:32:45 -0700

    working on structuring data on the back end and putting it in convenient format for work

--- server/index.js
+++ server/index.js
@@ -13,6 +13,28 @@ app.use(cors());
 
 const apiKey = 'DwLaoIRBYFlqE7JcaTMF7klB7LKzXkJueUThMPKv';
 
+//variables 
+let products = [];
+class Product {
+    constructor(id, image) {
+        this.id = id;
+        this.image = image;
+        this.colors = []
+    }
+}
+class Option {
+    constructor (id, price, size, image, avl, ) {
+        this.variant_id = id;
+        this.price = price;
+        this.size = size
+        this.image = image;
+        this.availability = avl
+    }
+}
+
+
+
+// functions
 async function getPrintfulData(endpoint) {
     const response = await axios.get(`https://api.printful.com/${endpoint}`, {
         headers: {
@@ -21,34 +43,37 @@ async function getPrintfulData(endpoint) {
     });
     return (response.data.result);
 }
-
-app.get('/products', async (req, res) => {
+async function getProducts()  {
     const data = await getPrintfulData('store/products');
-    res.send(JSON.stringify(data));
-});
-app.get('/product-variants', async (req, res) => {
-    const data = await getPrintfulData('store/products');
-    const ids = [];
-    data.forEach((product) => {
-        ids.push(product.id);
-    }
-    );
-    const variants = [];
-    for (let i = 0; i < ids.length; i++) {
-        const response = await getPrintfulData(`store/products/${ids[i]}`);
-        variants.push(response);
-    }
-    variants.map(item => {
-        item.variant_options = []
-        item.sync_variants.map(i => {
-            item.variant_options.push(i.variant_id)
+    
+    data.forEach((p) => {
+        products.push(new Product(p.id, p.thumbnail_url))
+    });
+    for (let i = 0; i < products.length; i++) {
+        const response = await getPrintfulData(`store/products/${products[i].id}`);
+        let variants = response.sync_variants
+        products[i].name = response.sync_product.name
+        products[i].options = [];
+        variants.map(option => {
+            products[i].colors.push(option.color)
+            products[i].options.push(option.variant_id)
         })
-    })
-    res.send(JSON.stringify(variants));
+        products[i].colors = products[i].colors.filter((value, index) => products[i].colors.indexOf(value) === index);
+    }
+}
+
+//endpoints
+app.get('/products', async (req, res) => {
+    const data = await getProducts()
+    res.send(JSON.stringify(products));
 });
 
 app.get('/variants', async (req, res) => {
-    console.log(req.body)
+
+});
+
+app.get('/variants', async (req, res) => {
+
 });
 
 app.listen(port, () => {
